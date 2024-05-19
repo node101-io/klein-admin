6f5fb510dd7a1f2c789ecb60c7edf956adbce60d
YakupAltay <yakup.altay@boun.edu.tr>
2024-05-19 03:26:45 +0300

    fix: Update junk code

--- controllers/api/notifications/get.js
+++ controllers/api/notifications/get.js
@@ -6,18 +6,15 @@ let lastRequestTime = 0;
 let lastRequestResponse = null;
 
 module.exports = (req, res) => {
-  console.log(lastRequestTime, 'in')
   if (lastRequestTime > Date.now() - TEN_SECONDS_IN_MS)
     return res.json(lastRequestResponse);
-  console.log(lastRequestTime, 'out')
+
   lastRequestTime = Date.now();
 
-  const filters = {
+  Notification.findNotificationsByFilters({
     is_deleted: false,
     is_published: true
-  };
-
-  Notification.findNotificationsByFilters(filters, (err, data) => {
+  }, (err, data) => {
     if (err) return res.json({
       success: false,
       error: err
